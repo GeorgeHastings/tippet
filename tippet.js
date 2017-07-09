06c7f02ba82b616250bed2c9ae153a000359c8c2
georgehastings <g.hastings3@gmail.com>
2017-07-09 18:18:27 -0400

    es6 updates

--- tippet.js
+++ tippet.js
@@ -6,30 +6,28 @@
   (global.tippet = factory());
 }(this, function () {
 
-var el;
-var theme = {background: '#fff', text: '#333'};
+let contents;
+let theme = {background: '#02142B', text: '#fff'};
 
 var genStyleSheet = function() {
-  var css = `
+  const css = `
     .tippet {
       position: absolute;
       display: inline-block;
-      padding: 20px;
+      padding: 10px;
       max-width: 20em;
       border-radius: 3px;
       font: inherit;
       font-size: 0.75em;
       line-height: 1.5em;
+      z-index: 9999;
       box-shadow: 0 1px 3px -1px rgba(0,0,0,.33);
-    }
-    *[data-tippet] {
-      cursor: help;
     }
     .tippet * {
       width: 100%;
     }`;
-  var head = document.head || document.getElementsByTagName('head')[0];
-  var style = document.createElement('style');
+  const head = document.head || document.getElementsByTagName('head')[0];
+  const style = document.createElement('style');
   style.type = 'text/css';
   if (style.styleSheet){
     style.styleSheet.cssText = css;
@@ -44,37 +42,49 @@ var getBaseStyle = function(theme) {
 };
 
 const tippet = {
+  element: function() {
+    if(document.getElementById('Tippet')) {
+      document.getElementById('Tippet').innerHTML = contents;
+      return document.getElementById('Tippet');
+    }
+    else {
+      const wrap = document.createElement('div');
+      const tip = `<div id="Tippet" class="tippet">${contents}</div>`;
+      wrap.innerHTML = tip;
+      const newTip = wrap.childNodes[0];
+      document.body.appendChild(newTip);
+      return newTip;
+    }
+  },
+
   position: function(event) {
-    var winWidth = window.innerWidth;
-    var winHeight = window.innerHeight;
-    var offPageRight = event.pageX + (el.offsetWidth + 10) > winWidth;
-    var offPageBottom = event.pageY + (el.offsetHeight + 10) > winHeight;
-    var style = `
+    const el = tippet.element();
+    const winWidth = window.innerWidth;
+    const winHeight = window.innerHeight;
+    const offPageRight = event.pageX + (el.offsetWidth + 10) > winWidth;
+    const offPageBottom = (event.pageY - document.body.scrollTop) + (el.offsetHeight + 10) > winHeight;
+    const style = `
       left: ${offPageRight ? -el.offsetWidth - 10 : 10}px;
       top: ${offPageBottom ? -el.offsetHeight - 10 : 10}px;
       transform: translate3d(${event.pageX}px, ${event.pageY}px, 0);
       ${getBaseStyle(theme)}
     `;
-    el.style = style;
+    el.setAttribute('style', style);
   },
 
-  show: function(content) {
-    var wrap = document.createElement('div');
-    var tip = `<div id="Tippet" class="tippet">${content}</div>`;
-    wrap.innerHTML = tip;
-    var newTip = wrap.childNodes[0];
-    document.body.appendChild(newTip);
-    el = newTip;
+  show: function() {
+    tippet.element().style.display = 'inline-block';
     bindEvents();
   },
 
   hide: function() {
-    document.getElementById('Tippet').remove();
+    tippet.element().style.display = 'none';
   },
 
   render: function() {
-    var content = this.getAttribute('data-tippet');
-    tippet.show(content);
+    const content = this.getAttribute('data-tippet');
+    contents = content;
+    tippet.show();
   },
 
   init: function(setTheme) {
@@ -89,7 +99,7 @@ const tippet = {
 };
 
 var bindEvents = function() {
-  var tippets = document.querySelectorAll('[data-tippet]');
+  const tippets = document.querySelectorAll('[data-tippet]');
   for (var i = 0; i < tippets.length; i++) {
     tippets[i].onmouseenter = tippet.render;
     tippets[i].onmousemove = tippet.position;
